b4acbd48f33f5d006cedcc21648eb394a544e199
JWL <jwl.vdonsk@gmail.com>
2023-06-23 16:13:32 +0300

    PHP for begginers, episode 20

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 require ('functions.php');
-require ('router.php');
+//require ('router.php');
+require('Database.php');
+
+$config = require('config.php');
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+
+//dd($id);
+
+$query = "select * from posts where id = :id";
+
+$posts = $db-> query($query, [':id'=> $id])->fetchAll();
+
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
